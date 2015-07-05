e8fc4165430d44a7e1bdeb3da7fce1b4382ff436
Sebastián Osorio <svzosorio@boolinc.co>
2015-07-05 18:29:53 -0500

    add getLeague method

--- app/js/singup/services/summoner.service.js
+++ app/js/singup/services/summoner.service.js
@@ -8,15 +8,27 @@
 
     function Summoner($http,api, apiKey) {
 
-        this.get = function(summonerName){
+        this.get = function(summoner){
             return $http({
                 method: 'get',
-                url: api.summonerByName(summonerName),
+                url: api.summonerByName(summoner),
                 params:{
                     'api_key': apiKey
                 }
             }).then(function(response){
-                return response.data;
+                return response.data[summoner.replace(/\s/g, '').toLowerCase()];
+            });
+        };
+
+        this.getLeague = function(summoner){
+            return $http({
+                method: 'get',
+                url: api.summonerLeague(summoner),
+                params:{
+                    'api_key': apiKey
+                }
+            }).then(function(response){
+                return response.data[summoner];
             });
         };
     }
