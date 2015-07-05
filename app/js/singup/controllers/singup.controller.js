0a8e75b186260e2033cd9ee888743d5b69dc438f
Sebastián Osorio <svzosorio@boolinc.co>
2015-07-05 07:11:06 -0500

    initialize summoners whit profile icon

--- app/js/singup/controllers/singup.controller.js
+++ app/js/singup/controllers/singup.controller.js
@@ -2,10 +2,28 @@
     'use strict';
 
     angular
-        .module('lolTot.singup')
-        .controller('SingupController', SingupController);
+    .module('lolTot.singup')
+    .controller('SingupController', SingupController);
 
     function SingupController($log) {
-        $log.log('SingupController');
+        var vm = this;
+
+        vm.summoners = [
+            {
+                profileIconId: 657
+            },
+            {
+                profileIconId: 658
+            },
+            {
+                profileIconId: 659
+            },
+            {
+                profileIconId: 660
+            },
+            {
+                profileIconId: 661
+            }
+        ];
     }
 })();
