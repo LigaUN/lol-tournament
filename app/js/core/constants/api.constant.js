c92be20538a9c622b3936aaea6c62f9e3ae0b71c
Sebastián Osorio <svzosorio@boolinc.co>
2015-07-05 19:43:06 -0500

    add summoners to the form

--- app/js/core/constants/api.constant.js
+++ app/js/core/constants/api.constant.js
@@ -10,7 +10,7 @@
             route: function(path){
                 var url = this.url;
                 return '{0}://{1}/{2}'.format(
-                    url.protocol, url.host,path
+                    url.protocol, url.host, path
                 );
             },
             id: function(url){
@@ -25,8 +25,12 @@
         // Root: Url id
         id: apiRouter.id,
 
+        //summoner
         summonerByName: function(name){
             return apiRouter.route('v1.4/summoner/by-name/{0}'.format(name));
+        },
+        summonerLeague: function(id){
+            return apiRouter.route('v2.5/league/by-summoner/{0}/entry'.format(id));
         }
     };
 
--- app/js/singup/controllers/singup.controller.js
+++ app/js/singup/controllers/singup.controller.js
@@ -5,10 +5,11 @@
     .module('lolTot.singup')
     .controller('SingupController', SingupController);
 
-    function SingupController($log, Summoner) {
+    function SingupController($log, $timeout, Summoner) {
         var vm = this;
 
-        vm.summoners = [
+        vm.getSummoner = getSummoner;
+        vm.summonersForm = [
             {
                 profileIconId: 657
             },
@@ -26,9 +27,19 @@
             }
         ];
 
-        Summoner.get('Señor Galleto').then(function(data){
-            console.log(data);
-            vm.summoners[0].data = data['señorgalleto'];
-        });
+        function getSummoner (summoner, index){
+            Summoner.get(summoner).then(function(data){
+                vm.summonersForm[index].data = data;
+                Summoner.getLeague(data.id)
+                .then(function(data){
+                    vm.summonersForm[index].data.league = data;
+                })
+                .catch(function(data){
+                    if(data.status === 404){
+                        vm.summonersForm[index].data.league = 'unranked';
+                    }
+                });
+            });
+        }
     }
 })();
