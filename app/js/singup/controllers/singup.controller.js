ca60b5cec8d7c4d486786dd21fe8cc23a531f166
Sebastián Osorio <svzosorio@boolinc.co>
2015-07-06 00:37:04 -0500

    delete if array is unique method

--- app/js/singup/controllers/singup.controller.js
+++ app/js/singup/controllers/singup.controller.js
@@ -81,19 +81,5 @@
                 });
             });
         }
-
-        function ifArrayIsUnique(arr) {
-            var map = {}, i, size;
-
-            for (i = 0, size = arr.length; i < size; i++){
-                if (map[arr[i]]){
-                    return false;
-                }
-
-                map[arr[i]] = true;
-            }
-
-            return true;
-        }
     }
 })();
